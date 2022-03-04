4dcb770a3c672f5ec28991c373a7e0e1af3d70e7
Kzlyr Shaira Manejo <99744382+kezulo@users.noreply.github.com>
2022-03-03 20:28:46 -0800

    ch3_ex1

--- Lecture1/Reading-Assignment/ch3_ex1.c
+++ Lecture1/Reading-Assignment/ch3_ex1.c
@@ -3,8 +3,8 @@
 int main (void){
 
 	//declare the data type of the variables that will be used
-	int i, j;		  //variables i and j are specified as type int which means they'll hold integer values
-	float x, y;		//variables x and y are specified as type float which means they'll hold float values
+	int i, j;	//variables i and j are specified as type int which means they'll hold integer values
+	float x, y;	//variables x and y are specified as type float which means they'll hold float values
 
 	//assign values to each variables
 	i = 10;		
@@ -13,6 +13,6 @@ int main (void){
 	y = 5527.0f;
 
 	//prints all the values of the variables 
-	printf ("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);  //the four conversion specifications are replaced by the values of variables 1, j, x, and y
+	printf ("i = %d, j = %d, x = %f, y = %f\n", i, j, x, y);  //the four conversion specifications are replaced by the values of the variables i, j, x, and y
 
 }
