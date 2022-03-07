1e77871731c8d105aa5ed793caa0638e324d2ea0
Kzlyr Shaira Manejo <99744382+kezulo@users.noreply.github.com>
2022-03-07 12:30:00 -0800

    Add files via upload

---
+++ Lecture1/Assignments/assignment_lec2_c2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int difference;
+
+	//COMPUTE RESULT
+	difference = 87-15;
+
+	//DISPLAY RESULT
+	printf ("The difference of 87 and 15 is %d", difference); 
+
+}
